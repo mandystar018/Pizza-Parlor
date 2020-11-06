127923a725532d853ad4bf6caed9029d61905ac7
Amanda Otero <mandystar018@gmail.com>
2020-11-06 08:40:58 -0800

    Add Pseudo code on the scripts.js to have an idea of the constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-$(document).ready(function(){
-
-});
+//Describe: Pizza()
+//test: "It should create an order of pizza with toppings and size"
+//code: let pizzaOrder = new Pizza(topping, size);
+//Expect: pizzaOrder("peperonni", "small");
