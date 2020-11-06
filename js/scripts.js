5e55badb00ff137042d1666051586bc12975caf2
Amanda Otero <mandystar018@gmail.com>
2020-11-06 10:50:13 -0800

    Test number 5 if the user doesnt enter pizza topping or size message will appear

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,8 @@
-//Describe: Pizza()
-//test: "It should create an order of pizza with toppings and size"
-//code: let pizzaOrder = new Pizza(topping, size);
-//Expect: pizzaOrder("peperonni", "small");
-
-
 function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
   this.price = 0;
 }
-
-//cost(pizza)
-// if toppings === "pepperoni" and size === "small"
-//  this.price = 15;
-// this.pizza.push(pizza)
-// else if toppings === "pepperoni" and size === "large"
-// this.price = 20
 
 Pizza.prototype.cost = function() {
   if (this.toppings === "pepperoni" && this.size === "small") {
@@ -26,5 +13,7 @@ Pizza.prototype.cost = function() {
     return this.price += 10;
   } else if (this.toppings === "cheese" && this.size === "large") {
     return this.price += 17;
+  } else {
+    return "Enter a topping and size"
   }
 }
---
+++ js/test.js
@@ -0,0 +1,14 @@
+//Describe: Pizza()
+//test: "It should create an order of pizza with toppings and size"
+//code: let pizzaOrder = new Pizza(topping, size);
+//Expect: pizzaOrder("peperonni", "small");
+
+//cost(pizza)
+// if toppings "pepperoni" and size "small"
+//price = 15;
+// else if toppings "pepperoni" and size "large"
+//price = 20;
+//else if topping "cheese" and size "small"
+//price = 10
+//else if topping "cheese" and size "large"
+//price = 17
